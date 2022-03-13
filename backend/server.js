55ab3f97d8f4acbc3b532f84fdb5d630f0a43a8e
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-03-13 12:33:42 +0700

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const { response } = require('express')
 const PORT = process.env.PORT || 5000
 
 // Connect to database
@@ -14,13 +16,23 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk api' })
-})
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    response.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk api' })
+  })
+}
 
 app.use(errorHandler)
 
