26460ab35995238a82b0b2e14d83f3357a01c6f7
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-03-13 12:43:25 +0700

    Fix bugs

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
+// update
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
